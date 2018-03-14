0d46140b135dcbd2f31e191209857ec34be7e4a4
henry <henry.geng@aliyun.com>
2018-03-14 16:33:29 +0800

    商户端更新

---
+++ app/src/main/java/com/huami/merchant/activity/exhibition/ExhibitionMainActivity.java
@@ -0,0 +1,15 @@
+package com.huami.merchant.activity.exhibition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.huami.merchant.R;
+
+public class ExhibitionMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exhibition_main);
+    }
+}
