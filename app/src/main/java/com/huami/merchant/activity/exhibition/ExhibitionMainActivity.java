41b88929e74830eb59423934a40c3084b3c9eb51
henry <henry.geng@aliyun.com>
2018-03-14 18:07:52 +0800

    商户端更新

--- app/src/main/java/com/huami/merchant/activity/exhibition/ExhibitionMainActivity.java
+++ app/src/main/java/com/huami/merchant/activity/exhibition/ExhibitionMainActivity.java
@@ -1,15 +1,35 @@
 package com.huami.merchant.activity.exhibition;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import com.huami.merchant.R;
+import com.huami.merchant.mvpbase.BasePresenter;
+import com.huami.merchant.mvpbase.MvpBaseActivity;
+import butterknife.BindView;
 
-public class ExhibitionMainActivity extends AppCompatActivity {
+public class ExhibitionMainActivity extends MvpBaseActivity {
+    @BindView(R.id.bottom_nav)
+    RadioGroup bottom_nav;
+    @Override
+    protected BasePresenter getPresenter() {
+        return null;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exhibition_main);
+    protected int initLayout() {
+        return R.layout.activity_exhibition_main;
+    }
+
+    @Override
+    protected void initData() {
+    }
+
+    @Override
+    protected void initView() {
+        bottom_nav.check(R.id.home);
+
+    }
+    @Override
+    protected void setToolBar(TextView t_name, TextView t_menu) {
+
     }
 }
---
+++ app/src/main/java/com/huami/merchant/fragment/exhibition/ExhibitionHomeFragment.java
@@ -0,0 +1,25 @@
+package com.huami.merchant.fragment.exhibition;
+
+import com.huami.merchant.mvpbase.BasePresenter;
+import com.huami.merchant.mvpbase.MvpBaseFragment;
+
+/**
+ * Created by henry on 2018/3/14.
+ */
+
+public class ExhibitionHomeFragment extends MvpBaseFragment {
+    @Override
+    protected BasePresenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int initLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
