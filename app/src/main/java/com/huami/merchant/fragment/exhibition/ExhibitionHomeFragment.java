92b11ded51c2106451a11c50ad279c136f1dd43b
henry <henry.geng@aliyun.com>
2018-03-18 15:13:31 +0800

    修改首页轮播图

--- app/src/main/java/com/huami/merchant/fragment/exhibition/ExhibitionHomeFragment.java
+++ app/src/main/java/com/huami/merchant/fragment/exhibition/ExhibitionHomeFragment.java
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide;
 import com.huami.merchant.R;
 import com.huami.merchant.activity.exhibition.ExhibitionMainActivity;
 import com.huami.merchant.activity.task.TaskEditActivity;
+import com.huami.merchant.activity.web.AgentWebActivity;
 import com.huami.merchant.fragment.exhibition.presenter.ExhibitionPresenter;
 import com.huami.merchant.fragment.exhibition.view.BannerViewInter;
 import com.huami.merchant.mvpbase.BaseConsts;
@@ -22,7 +23,7 @@ import cn.bingoogolapple.bgabanner.BGABanner;
  * Created by henry on 2018/3/14.
  */
 
-public class ExhibitionHomeFragment extends MvpBaseFragment<ExhibitionPresenter,ExhibitionHomeFragment> implements BannerViewInter{
+public class ExhibitionHomeFragment extends MvpBaseFragment<ExhibitionPresenter,ExhibitionHomeFragment> implements BGABanner.Delegate<ImageView,Integer>,BannerViewInter{
     @BindView(R.id.ad_banner)
     BGABanner ad_banner;
     @BindView(R.id.order_now)
@@ -32,6 +33,7 @@ public class ExhibitionHomeFragment extends MvpBaseFragment<ExhibitionPresenter,
     @BindView(R.id.sheet_form)
     LinearLayout sheet_form;
     private List<String> bannerList=new ArrayList<>();
+    private List<Integer> banners=new ArrayList<>();
     @Override
     protected ExhibitionPresenter getPresenter() {
         return new ExhibitionPresenter();
@@ -44,7 +46,7 @@ public class ExhibitionHomeFragment extends MvpBaseFragment<ExhibitionPresenter,
     protected void initData() {
         showLoading();
         float width = DisplayUtil.getScreenWidthPixels()*1.0f;
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams((int) width, (int) (width / 5*2.5));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams((int) width, (int) (width / 1.56));
         ad_banner.setLayoutParams(params);
         try {
             presenter.getBanner(BaseConsts.MAIN_TOPIC,bannerList);
@@ -56,10 +58,14 @@ public class ExhibitionHomeFragment extends MvpBaseFragment<ExhibitionPresenter,
     @Override
     public void loadSuccess() {
         endLoading();
-        ad_banner.setData(bannerList,new ArrayList<String>());
-        ad_banner.setAdapter(new BGABanner.Adapter<ImageView, String>() {
+        banners.add(R.mipmap.banner_one);
+        banners.add(R.mipmap.banner_second);
+        banners.add(R.mipmap.banner_third);
+        ad_banner.setDelegate(this);
+        ad_banner.setData(banners,new ArrayList<String>());
+        ad_banner.setAdapter(new BGABanner.Adapter<ImageView, Integer>() {
             @Override
-            public void fillBannerItem(BGABanner banner, ImageView itemView, String model, int position) {
+            public void fillBannerItem(BGABanner banner, ImageView itemView, Integer model, int position) {
                 Glide.with(itemView.getContext())
                         .load(model)
                         .placeholder(R.mipmap.theme_seize_pic)
@@ -86,5 +92,11 @@ public class ExhibitionHomeFragment extends MvpBaseFragment<ExhibitionPresenter,
     @OnClick(R.id.order_now)
     public void goOrder(){
         startActivity(getActivity(), TaskEditActivity.class);
+    }
+
+    private String[] urls = new String[]{"http://yygj.darenlaiye.com/rw/1.html", "http://yygj.darenlaiye.com/rw/2.html", "http://yygj.darenlaiye.com/rw/3.html"};
+    @Override
+    public void onBannerItemClick(BGABanner banner, ImageView itemView, Integer model, int position) {
+        startActivity(getActivity(), AgentWebActivity.class,"html_url",urls[position]);
     }
 }
